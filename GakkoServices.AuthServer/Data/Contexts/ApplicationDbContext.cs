4cd9bdcbd16af59c5568fd85fff23248cdab2daa
Andrew Godfroy <andrew.godfroy@killerrin.com>
2019-02-27 00:28:49 -0500

    Adding ApplicationDbContext

---
+++ GakkoServices.AuthServer/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using GakkoServices.AuthServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GakkoServices.AuthServer.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}

b15e5c29edcd5f485cb214c69a80f544efd1b732
Andrew Godfroy <andrew.godfroy@killerrin.com>
2019-03-10 01:01:15 -0500

    Removed Whitespace

--- GakkoServices.AuthServer/Startup.cs
+++ GakkoServices.AuthServer/Startup.cs
@@ -49,7 +49,6 @@ namespace GakkoServices.AuthServer
             Configuration = builder.Build();
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {

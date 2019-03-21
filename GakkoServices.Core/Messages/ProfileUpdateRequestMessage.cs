3012799e53deabe53025df85a996a69450b0a50a
Patrick Gingras <775.pg.12@gmail.com>
2019-03-21 12:56:24 -0400

    added ProfileUpdateRequestMessage

---
+++ GakkoServices.Core/Messages/ProfileUpdateRequestMessage.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace GakkoServices.Core.Messages
+{
+    public class ProfileUpdateRequestMessage
+    {
+        // TODO: Identifier and partial profile object maybe?
+    }
+}

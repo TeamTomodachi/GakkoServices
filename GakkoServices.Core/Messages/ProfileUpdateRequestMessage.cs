c8819c206faec661dca2908f85ae8d520c3b942d
ging0044 <775.pg.12@gmail.com>
2019-03-23 23:25:07 -0400

    new profile update request fields

--- GakkoServices.Core/Messages/ProfileUpdateRequestMessage.cs
+++ GakkoServices.Core/Messages/ProfileUpdateRequestMessage.cs
@@ -5,5 +5,13 @@ namespace GakkoServices.Core.Messages
     public class ProfileUpdateRequestMessage
     {
         // TODO: Identifier and partial profile object maybe?
+
+        public Guid Id;
+        public Guid? UserAccountId;
+        public Guid? PogoTeamId;
+
+        public string PogoUsername;
+        public string PogoTrainerCode;
+        public int? PogoLevel;
     }
 }

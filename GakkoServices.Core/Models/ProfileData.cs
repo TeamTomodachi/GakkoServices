5df44cc7d1f8248dabe1481ace1a7d378e96bc21
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-04-13 13:56:12 -0400

    Update ProfileData.cs

--- GakkoServices.Core/Models/ProfileData.cs
+++ GakkoServices.Core/Models/ProfileData.cs
@@ -10,6 +10,7 @@ namespace GakkoServices.Core.Models
         public int Gender { get; set; }
         public Guid TeamId { get; set; }
         public int Level { get; set; }
+
         public Guid FeaturedPokemon1 { get; set; }
         public Guid FeaturedPokemon2 { get; set; }
         public Guid FeaturedPokemon3 { get; set; }
